c07cbaa4877b278366454cac708aaa88ce83cc40
Carl <carllindqvist93@gmail.com>
2024-11-06 11:02:27 +0100

    Tester klara

---
+++ EmailProviderRika.Tests/Models/ServiceBusTestMessage.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace EmailProviderRika.Tests.Models;
+
+public class ServiceBusTestMessage
+{
+    public string Email { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+}
